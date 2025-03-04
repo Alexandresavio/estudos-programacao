18980476586c459f96f8aef880e526c09f27ecd5
Savio Alexandre <alexandresaviosouza@gmail.com>
2025-03-03 22:19:35 -0300

    adicionando arquivos da aula de estrutura sequencial

---
+++ curso_programacao/src/estrutura_sequencial/EstruturaSequencial.java
@@ -0,0 +1,30 @@
+package estrutura_sequencial;
+
+public class EstruturaSequencial {
+	/* Por que o nome "estrutura sequencial"?
+	 * Para enfatizar que os comandos do algoritmo executam em sequência, de cima para baixo.
+	 * Um algoritmo deve obedecer uma sequência lógica adequada para cumprir seu papel 
+	 */
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 5;
+
+		int soma = x + y;
+		System.out.println("A soma total é: "+soma);
+		
+		/*
+		 * Este seria um código com a sequencia logica errada
+		 * 
+		 * int soma = x + y;
+		 * 
+		 * int x = 10;
+		 * int y = 5;
+		 * System.out.println("A soma total é: "+soma);
+		 */
+		
+		
+
+	}
+
+}
